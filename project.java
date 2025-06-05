af3711955e9840ee23fb987b0309788f3bd11b12
kimiaarabi <arabiikimia@gmail.com>
2025-06-05 16:28:12 +0330

    shopmusic getter setter added

--- project.java
+++ project.java
@@ -332,4 +332,40 @@ class ShopMusic{
         this.price = price;
         this.downloadCount = downloadCount;
     }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+    public double getRating() {
+        return rating;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getDownloadCount() {
+        return downloadCount;
+    }
+    public void setDownloadCount(int downloadCount) {
+        this.downloadCount = downloadCount;
+    }
+    public boolean isDownloaded() {
+        return isDownloaded;
+    }
+    public void setDownloaded(boolean downloaded) {
+        isDownloaded = downloaded;
+    }
 }
